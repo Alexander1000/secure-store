2f7d9c6ed39812c972114667ed76a231186bcdaf
Alexander Dankovtsev <a.dankovtsev@mail.ru>
2022-03-05 11:26:56 +0300

    config

--- include/secure-store/config.h
+++ include/secure-store/config.h
@@ -7,6 +7,8 @@ namespace SecureStore
     {
     public:
         Config();
+    private:
+        void initialize();
     };
 }
 
---
+++ src/config.cpp
@@ -0,0 +1,13 @@
+#include <secure-store.h>
+
+namespace SecureStore
+{
+    Config::Config()
+    {
+        this->initialize();
+    }
+
+    void Config::initialize()
+    {
+    }
+}
