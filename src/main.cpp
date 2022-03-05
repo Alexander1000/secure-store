692a4894409f4ca2eb53a1fa4cccb1ca59e2bc48
Alexander Dankovtsev <a.dankovtsev@mail.ru>
2022-03-05 08:14:35 +0300

    detect home dir

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,13 @@
+#include <iostream>
+#include <unistd.h>
+#include <pwd.h>
+
 int main() {
+    struct passwd *pw = getpwuid(getuid());
+
+    const char *homedir = pw->pw_dir;
+
+    std::cout << "Home dir: " << homedir << std::endl;
+
     return 0;
 }
