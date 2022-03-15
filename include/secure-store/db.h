3bcc5c4db13b035a2a08ccd6ece21f7e34a99164
Alexander Dankovtsev <a.dankovtsev@mail.ru>
2022-03-15 08:26:25 +0300

    define record class

--- include/secure-store/db.h
+++ include/secure-store/db.h
@@ -1,6 +1,8 @@
 #ifndef H_SECURE_STORE_DB
 #define H_SECURE_STORE_DB
 
+#include <string>
+
 namespace SecureStore
 {
     class DB
@@ -8,6 +10,17 @@ namespace SecureStore
     public:
         DB();
         void createEmpty();
+    };
+
+    class Record
+    {
+    public:
+        Record();
+
+    private:
+        std::string* name;
+        std::string* user;
+        std::string* password;
     };
 }
 
