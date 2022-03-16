330a89e7a9b9058b92ef274cd2f88201d77f3c15
Alexander Dankovtsev <a.dankovtsev@mail.ru>
2022-03-16 11:33:49 +0300

    generate bitmask

--- src/storage/pack.cpp
+++ src/storage/pack.cpp
@@ -4,6 +4,28 @@ namespace SecureStore::Storage
 {
     DataPack* pack(Record* record)
     {
+        if (record == nullptr) {
+            return nullptr;
+        }
+
+        uint8_t bitmask = 0;
+
+        if (record->getUser() != nullptr) {
+            bitmask &= DATA_PACK_USER;
+        }
+
+        if (record->getPassword() != nullptr) {
+            bitmask &= DATA_PACK_PASSWORD;
+        }
+
+        if (record->getComment() != nullptr) {
+            bitmask &= DATA_PACK_COMMENT;
+        }
+
+        if (record->getKeywords() != nullptr && record->getKeywords()->size() > 0) {
+            bitmask &= DATA_PACK_KEYWORDS;
+        }
+
         int size = 0; // 4 (id)
         return nullptr;
     }
