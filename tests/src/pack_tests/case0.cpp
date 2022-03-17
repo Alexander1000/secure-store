f7101c9608d895cdedf24b776e205b029dde9321
Alexander Dankovtsev <a.dankovtsev@mail.ru>
2022-03-17 14:02:19 +0300

    tests

--- tests/src/pack_tests/case0.cpp
+++ tests/src/pack_tests/case0.cpp
@@ -1,6 +1,7 @@
 #include <tests.h>
 #include <secure-store.h>
 #include <cpp-unit-test.h>
+#include <list>
 #include <string>
 
 namespace SecureStoreTest
@@ -17,6 +18,20 @@ namespace SecureStoreTest
         record->setId(1);
         std::string name = "TestCase01";
         record->setName(&name);
+
+        std::string user = "some-login";
+        record->setUser(&user);
+
+        std::string password = "12345Qwerty!@#";
+        record->setPassword(&password);
+
+        std::string comment = "It is example record in secure store";
+        record->setComment(&comment);
+
+        std::list<std::string> keywords;
+        record->setKeywords(&keywords);
+
+        record->setCreateTime(187766224);
 
         SecureStore::Storage::DataPack *dataPack = SecureStore::Storage::pack(record);
 
