1dbe8dedee90b628433253a719044d1c59a54ba4
Alexander Dankovtsev <a.dankovtsev@mail.ru>
2022-03-17 15:56:29 +0300

    unpack method

--- src/storage/unpack.cpp
+++ src/storage/unpack.cpp
@@ -4,6 +4,13 @@ namespace SecureStore::Storage
 {
     Record* unpack(DataPack* dataPack)
     {
+        int length = dataPack->getLength();
+        void* rawData = dataPack->getData();
+
+        if (length == 0 || rawData == nullptr) {
+            return nullptr;
+        }
+
         return nullptr;
     }
 }
