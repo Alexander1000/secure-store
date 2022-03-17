33d970122b73b4a51760d9dcbef6bf3676ea8c64
Alexander Dankovtsev <a.dankovtsev@mail.ru>
2022-03-17 13:42:18 +0300

    pack record

--- tests/src/pack_tests/case0.cpp
+++ tests/src/pack_tests/case0.cpp
@@ -1,5 +1,7 @@
 #include <tests.h>
+#include <secure-store.h>
 #include <cpp-unit-test.h>
+#include <string>
 
 namespace SecureStoreTest
 {
@@ -9,7 +11,14 @@ namespace SecureStoreTest
         t = new CppUnitTest::TestCase("test case0");
         t->printTitle();
 
-        // do tests with asserts
+        SecureStore::Storage::Record *record;
+        record = new SecureStore::Storage::Record;
+
+        record->setId(1);
+        std::string name = "TestCase01";
+        record->setName(&name);
+
+        SecureStore::Storage::DataPack *dataPack = SecureStore::Storage::pack(record);
 
         t->finish();
 
