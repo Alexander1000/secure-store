291629e7108ae6b244c37a942f4c66369754425f
Alexander Dankovtsev <a.dankovtsev@mail.ru>
2022-03-21 14:54:32 +0300

    finilize

--- src/storage/db_encode.cpp
+++ src/storage/db_encode.cpp
@@ -79,6 +79,8 @@ namespace SecureStore::Storage
         buffer.write((char*) input->getData(), input->getLength());
         buffer.setPosition(0);
 
+        IOBuffer::IOMemoryBuffer outBuffer(BUFSIZE);
+
         while(1) {
             // Read in data in blocks until EOF. Update the ciphering with each read.
             // memcpy(in_buf, (uint8_t*) input->getData() + index * BUFSIZ, sizeof(uint8_t) * BUFSIZE);
@@ -95,32 +97,35 @@ namespace SecureStore::Storage
                 EVP_CIPHER_CTX_cleanup(ctx);
                 // cleanup(params, ifp, ofp, ERR_EVP_CIPHER_UPDATE);
             }
-//            fwrite(out_buf, sizeof(unsigned char), out_len, ofp);
+
+            outBuffer.write((char*) out_buf, out_len);
+            // fwrite(out_buf, sizeof(unsigned char), out_len, ofp);
 //            if (ferror(ofp)) {
 //                fprintf(stderr, "ERROR: fwrite error: %s\n", strerror(errno));
 //                EVP_CIPHER_CTX_cleanup(ctx);
 //                // cleanup(params, ifp, ofp, errno);
 //            }
-//            if (num_bytes_read < BUFSIZE) {
-//                /* Reached End of file */
-//                break;
-//            }
+            if (num_bytes_read < BUFSIZE) {
+                /* Reached End of file */
+                break;
+            }
         }
 
         /* Now cipher the final block and write it out to file */
-//        if(!EVP_CipherFinal_ex(ctx, out_buf, &out_len)){
-//            fprintf(stderr, "ERROR: EVP_CipherFinal_ex failed. OpenSSL error: %s\n",
-//                    ERR_error_string(ERR_get_error(), NULL));
-//            EVP_CIPHER_CTX_cleanup(ctx);
-//            // cleanup(params, ifp, ofp, ERR_EVP_CIPHER_FINAL);
-//        }
-//        fwrite(out_buf, sizeof(unsigned char), out_len, ofp);
+        if(!EVP_CipherFinal_ex(ctx, out_buf, &out_len)){
+            fprintf(stderr, "ERROR: EVP_CipherFinal_ex failed. OpenSSL error: %s\n",
+                    ERR_error_string(ERR_get_error(), NULL));
+            EVP_CIPHER_CTX_cleanup(ctx);
+            // cleanup(params, ifp, ofp, ERR_EVP_CIPHER_FINAL);
+        }
+        outBuffer.write((char*) out_buf, out_len);
+        // fwrite(out_buf, sizeof(unsigned char), out_len, ofp);
 //        if (ferror(ofp)) {
 //            fprintf(stderr, "ERROR: fwrite error: %s\n", strerror(errno));
 //            EVP_CIPHER_CTX_cleanup(ctx);
 //            // cleanup(params, ifp, ofp, errno);
 //        }
-//        EVP_CIPHER_CTX_cleanup(ctx);
+        EVP_CIPHER_CTX_cleanup(ctx);
 
         return nullptr;
     }
