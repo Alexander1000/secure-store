a55f245644364adad132857fe7245dc83fa040a0
Alexander Dankovtsev <a.dankovtsev@mail.ru>
2022-03-21 17:09:43 +0300

    add encrypt decrypt method

---
+++ src/storage/encrypt_decrypt.cpp
@@ -0,0 +1,9 @@
+#include <secure-store.h>
+
+namespace SecureStore::Storage
+{
+    void encrypt_decrypt(cipher_params_t* params, DataPack* input, DataPack* output)
+    {
+        // do something
+    }
+}
