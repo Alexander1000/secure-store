2c9425b584b3c601509e9d804c1a901a70eced3a
Alexander Dankovtsev <a.dankovtsev@mail.ru>
2022-03-21 08:12:48 +0300

    pack secrets in heap

--- src/storage/main.cpp
+++ src/storage/main.cpp
@@ -35,6 +35,44 @@ namespace SecureStore::Storage
 
         void* rawData = malloc(headerSize + heapSize);
 
+        uint16_t heapOffset = 0;
+        bool first = true;
+        uint16_t index = 0;
+
+        // pack all secrets
+        for (auto & dbRecord : *this->records) {
+            if (dbRecord->getRecord() != nullptr) {
+                DataPack* dp = SecureStore::Storage::pack(dbRecord->getRecord());
+                uint16_t length = dp->getLength();
+
+                // write heap
+                memcpy((uint8_t*) rawData + heapSize + heapOffset, dp->getData(), length);
+
+                uint16_t indexOffset = index * 5;
+
+                // write header
+                uint16_t pointer;
+                if (first) {
+                    first = false;
+                    pointer = dataPackList.size();
+                } else {
+                    pointer = heapOffset;
+                }
+                // write offset(count) in header
+                memcpy((uint8_t*) rawData + indexOffset, &pointer, sizeof(uint16_t));
+                // write length data in header
+                memcpy((uint8_t*) rawData + indexOffset + sizeof(uint16_t), &length, sizeof(uint16_t));
+                // write status in header
+                uint8_t status = dbRecord->getStatus();
+                memcpy((uint8_t*) rawData + indexOffset + 2 * sizeof(uint16_t), &status, sizeof(uint8_t));
+
+                // move offset in next block
+                heapOffset += length;
+                // move offset in header
+                index++;
+            }
+        }
+
         return nullptr;
     }
 }
