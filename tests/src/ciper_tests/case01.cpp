0708d9e99d59f6cbd7d31792dd2ad17d12f17d2d
Alexander Dankovtsev <a.dankovtsev@mail.ru>
2022-03-21 15:23:12 +0300

    add record in db

--- tests/src/ciper_tests/case01.cpp
+++ tests/src/ciper_tests/case01.cpp
@@ -16,25 +16,27 @@ namespace SecureStoreTest
         record = new SecureStore::Storage::Record;
 
         record->setId(1);
-        std::string name = "PackAndUnpack with minimum data";
+        std::string name = "TestCase01";
         record->setName(&name);
+
+        std::string user = "some-login";
+        record->setUser(&user);
+
+        std::string password = "12345Qwerty!@#";
+        record->setPassword(&password);
+
+        std::string comment = "It is example record in secure store";
+        record->setComment(&comment);
+
+        std::list<std::string> keywords;
+        keywords.emplace_back("system");
+        keywords.emplace_back("critical");
+        record->setKeywords(&keywords);
 
         record->setCreateTime(187766224);
 
-        SecureStore::Storage::DataPack *dataPack = SecureStore::Storage::pack(record);
-
-        CppUnitTest::assertNotNull(t, dataPack);
-        CppUnitTest::assertEquals(t, 44, dataPack->getLength());
-
-        SecureStore::Storage::Record *unpackedRecord = SecureStore::Storage::unpack(dataPack);
-
-        CppUnitTest::assertEquals(t, record->getId(), unpackedRecord->getId());
-        CppUnitTest::assertEquals(t, *record->getName(), *unpackedRecord->getName());
-        CppUnitTest::assertNull(t, unpackedRecord->getUser());
-        CppUnitTest::assertNull(t, unpackedRecord->getPassword());
-        CppUnitTest::assertNull(t, unpackedRecord->getComment());
-        CppUnitTest::assertNull(t, unpackedRecord->getKeywords());
-        CppUnitTest::assertEquals(t, (int) record->getCreateTime(), (int) unpackedRecord->getCreateTime());
+        SecureStore::Storage::DB db;
+        db.addRecord(record);
 
         t->finish();
 
