c7d2170349294340de2654953ff33302701cfb5e
Alexander Dankovtsev <a.dankovtsev@mail.ru>
2022-03-21 14:49:00 +0300

    use io-buffer

--- src/storage/db_encode.cpp
+++ src/storage/db_encode.cpp
@@ -4,6 +4,7 @@
 #include <openssl/rand.h>
 #include <openssl/err.h>
 #include <memory>
+#include <io-buffer.h>
 
 #define BUFSIZE 100
 
@@ -41,39 +42,43 @@ namespace SecureStore::Storage
         params->cipher_type = EVP_aes_256_cbc();
 
         /* Allow enough space in output buffer for additional block */
-//        int cipher_block_size = EVP_CIPHER_block_size(params->cipher_type);
-//        unsigned char in_buf[BUFSIZE], out_buf[BUFSIZE + cipher_block_size];
-//
-//        int num_bytes_read, out_len;
-//        EVP_CIPHER_CTX *ctx;
-//
-//        ctx = EVP_CIPHER_CTX_new();
-//        if(ctx == NULL){
-//            fprintf(stderr, "ERROR: EVP_CIPHER_CTX_new failed. OpenSSL error: %s\n",
-//                    ERR_error_string(ERR_get_error(), NULL));
-//            // cleanup(params, ifp, ofp, ERR_EVP_CTX_NEW);
-//        }
-//
-//        /* Don't set key or IV right away; we want to check lengths */
-//        if(!EVP_CipherInit_ex(ctx, params->cipher_type, NULL, NULL, NULL, params->encrypt)){
-//            fprintf(stderr, "ERROR: EVP_CipherInit_ex failed. OpenSSL error: %s\n",
-//                    ERR_error_string(ERR_get_error(), NULL));
-//            // cleanup(params, ifp, ofp, ERR_EVP_CIPHER_INIT);
-//        }
-//
-//        OPENSSL_assert(EVP_CIPHER_CTX_key_length(ctx) == AES_256_KEY_SIZE);
-//        OPENSSL_assert(EVP_CIPHER_CTX_iv_length(ctx) == AES_BLOCK_SIZE);
-//
-//        /* Now we can set key and IV */
-//        if(!EVP_CipherInit_ex(ctx, NULL, NULL, params->key, params->iv, params->encrypt)){
-//            fprintf(stderr, "ERROR: EVP_CipherInit_ex failed. OpenSSL error: %s\n",
-//                    ERR_error_string(ERR_get_error(), NULL));
-//            EVP_CIPHER_CTX_cleanup(ctx);
-//            // cleanup(params, ifp, ofp, ERR_EVP_CIPHER_INIT);
-//        }
-//
-//        int index = 0;
-//
+        int cipher_block_size = EVP_CIPHER_block_size(params->cipher_type);
+        unsigned char in_buf[BUFSIZE], out_buf[BUFSIZE + cipher_block_size];
+
+        int num_bytes_read, out_len;
+        EVP_CIPHER_CTX *ctx;
+
+        ctx = EVP_CIPHER_CTX_new();
+        if(ctx == NULL){
+            fprintf(stderr, "ERROR: EVP_CIPHER_CTX_new failed. OpenSSL error: %s\n",
+                    ERR_error_string(ERR_get_error(), NULL));
+            // cleanup(params, ifp, ofp, ERR_EVP_CTX_NEW);
+        }
+
+        /* Don't set key or IV right away; we want to check lengths */
+        if(!EVP_CipherInit_ex(ctx, params->cipher_type, NULL, NULL, NULL, params->encrypt)){
+            fprintf(stderr, "ERROR: EVP_CipherInit_ex failed. OpenSSL error: %s\n",
+                    ERR_error_string(ERR_get_error(), NULL));
+            // cleanup(params, ifp, ofp, ERR_EVP_CIPHER_INIT);
+        }
+
+        OPENSSL_assert(EVP_CIPHER_CTX_key_length(ctx) == AES_256_KEY_SIZE);
+        OPENSSL_assert(EVP_CIPHER_CTX_iv_length(ctx) == AES_BLOCK_SIZE);
+
+        /* Now we can set key and IV */
+        if(!EVP_CipherInit_ex(ctx, NULL, NULL, params->key, params->iv, params->encrypt)){
+            fprintf(stderr, "ERROR: EVP_CipherInit_ex failed. OpenSSL error: %s\n",
+                    ERR_error_string(ERR_get_error(), NULL));
+            EVP_CIPHER_CTX_cleanup(ctx);
+            // cleanup(params, ifp, ofp, ERR_EVP_CIPHER_INIT);
+        }
+
+        int index = 0;
+
+        IOBuffer::IOMemoryBuffer buffer(BUFSIZE);
+        buffer.write((char*) input->getData(), input->getLength());
+        buffer.setPosition(0);
+
 //        while(1) {
 //            // Read in data in blocks until EOF. Update the ciphering with each read.
 //            memcpy(in_buf, (uint8_t*) input->getData() + index * BUFSIZ, sizeof(uint8_t) * BUFSIZE);
