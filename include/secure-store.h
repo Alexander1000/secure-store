21d0b19535af030f27b16ebfccd7c1112407eae5
Alexander Dankovtsev <a.dankovtsev@mail.ru>
2022-03-22 14:54:15 +0300

    add consts version

--- include/secure-store.h
+++ include/secure-store.h
@@ -1,6 +1,10 @@
 #ifndef H_SECURE_STORE
 #define H_SECURE_STORE
 
+#define VERSION_MAJOR 0
+#define VERSION_MINOR 0
+#define VERSION_PATCH 1
+
 #include <secure-store/config.h>
 #include <secure-store/menu.h>
 #include <secure-store/storage.h>
