74dfcbdcdba22d37312b28c031a9d0445390b1d4
Alexander Dankovtsev <a.dankovtsev@mail.ru>
2022-03-22 16:07:14 +0300

    md5 hash

--- src/storage/hash_md5.cpp
+++ src/storage/hash_md5.cpp
@@ -1,9 +1,20 @@
 #include <secure-store.h>
 
+#include <memory>
+#include <openssl/evp.h>
+
 namespace SecureStore::Storage
 {
     uint8_t* hash_md5(DataPack* input)
     {
-        return nullptr;
+        EVP_MD_CTX *ctx;
+        ctx = EVP_MD_CTX_new();
+
+        EVP_DigestInit(ctx, EVP_md5());
+        EVP_DigestUpdate(ctx, input->getData(), input->getLength());
+        uint8_t* md5 = (uint8_t*) malloc(16);
+        unsigned int size;
+        EVP_DigestFinal(ctx, md5, &size);
+        return md5;
     }
 }
