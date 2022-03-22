bedd96bff6ef93c5fa5f047fecf01c36d5507539
Alexander Dankovtsev <a.dankovtsev@mail.ru>
2022-03-22 15:42:33 +0300

    pack

--- src/storage/db_save.cpp
+++ src/storage/db_save.cpp
@@ -5,5 +5,6 @@ namespace SecureStore::Storage
     void DB::save(std::string* fileName, std::string* key)
     {
         this->fileName = fileName;
+        auto rawSecrets = this->pack();
     }
 }
