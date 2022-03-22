cb6ce25fcfc237b0183632a19051a4a4bb57dee2
Alexander Dankovtsev <a.dankovtsev@mail.ru>
2022-03-22 16:24:38 +0300

    generate header data

--- src/storage/db_save.cpp
+++ src/storage/db_save.cpp
@@ -9,6 +9,15 @@ namespace SecureStore::Storage
         uint8_t* headerData = (uint8_t*) malloc(24 * sizeof(uint8_t));
         memset(headerData, 0, 24 * sizeof(uint8_t));
 
+        // format
+        memcpy(headerData, this->format, 3 * sizeof(uint8_t));
+        // cipher algorithm
+        memcpy(headerData + 3, &this->cipherAlgorithm, sizeof(uint8_t));
+        // major version
+        memcpy(headerData + 4, &this->verMajor, sizeof(uint16_t));
+        // minor version
+        memcpy(headerData + 6, &this->verMinor, sizeof(uint16_t));
+
         auto rawSecrets = this->pack();
         auto md5Hash = hash_md5(rawSecrets);
     }
