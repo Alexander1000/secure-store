8e8edc318359b85eb9754955a08e551d2033c998
Alexander Dankovtsev <a.dankovtsev@mail.ru>
2022-03-23 11:45:08 +0300

    check xdb format

--- src/storage/db_open.cpp
+++ src/storage/db_open.cpp
@@ -5,11 +5,18 @@ namespace SecureStore::Storage
 {
     void DB::open(std::string *fileName, std::string *password)
     {
+        this->createEmpty();
+
         IOBuffer::IOFileReader fileReader(*fileName);
 
         auto headerData = (uint8_t*) malloc(DB_HEADER_BYTE_SIZE * sizeof(uint8_t));
         memset(headerData, 0, DB_HEADER_BYTE_SIZE * sizeof(uint8_t));
 
         fileReader.read((char*) headerData, DB_HEADER_BYTE_SIZE);
+
+        memcpy(this->format, headerData, 3 * sizeof(uint8_t));
+        if (std::string((char*) this->format) == "xdb") {
+            // it is xdb
+        }
     }
 }
