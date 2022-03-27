33162b2e1dbd250d0dd781b34d899e83ec30a7f5
Alexander Dankovtsev <a.dankovtsev@mail.ru>
2022-03-27 09:50:23 +0300

    add ncurses dep

--- src/view/run.cpp
+++ src/view/run.cpp
@@ -1,12 +1,17 @@
 #include <secure-store.h>
 
 #include <iostream>
+#include <ncurses.h>
 
 namespace SecureStore::View
 {
     void Application::run()
     {
-        // run application
+        initscr();                   // Переход в curses-режим
+        printw("Hello world!\n");  // Отображение приветствия в буфер
+        refresh();                   // Вывод приветствия на настоящий экран
+        getch();                     // Ожидание нажатия какой-либо клавиши пользователем
+        endwin();                    // Выход из curses-режима. Обязательная команда.
         TermSize size;
         this->termSize(&size);
         std::cout << "Cols: " << size.cols << "; rows: " << size.rows << std::endl;
