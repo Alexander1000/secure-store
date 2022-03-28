fee3b39e8f44c11bfc6a1f432b83ea06eed4b0fd
Alexander Dankovtsev <a.dankovtsev@mail.ru>
2022-03-28 08:13:53 +0300

    describe secret info

--- src/view/render.cpp
+++ src/view/render.cpp
@@ -24,13 +24,24 @@ namespace SecureStore::View
         p2.y = size.rows - 2;
         draw_line(&p1, &p2);
 
+        SecureStore::Storage::Record* selectedRecord = nullptr;
+
         int rows = 1;
         for (auto & dbRecord : *this->db->getRecords()) {
             if (this->currentRow + 1 == rows) {
                 mvaddstr(rows, 1, "-> ");
+                selectedRecord = dbRecord->getRecord();
             }
             mvaddstr(rows, 4, dbRecord->getRecord()->getName()->c_str());
             rows++;
+        }
+
+        if (selectedRecord != nullptr) {
+            mvprintw(1, breakLineX + 1, "ID: %d", selectedRecord->getId());
+            mvprintw(2, breakLineX + 1, "Name: %s", selectedRecord->getName()->c_str());
+            if (selectedRecord->getComment() != nullptr) {
+                mvprintw(3, breakLineX + 1, "Comment: %s", selectedRecord->getComment()->c_str());
+            }
         }
 
         refresh();                   // Вывод приветствия на настоящий экран
