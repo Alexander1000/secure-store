4e11ecbd97dff0ff9bd3eea3ec8e4460fc339a57
Alexander Dankovtsev <a.dankovtsev@mail.ru>
2022-04-03 10:17:10 +0300

    setup position controlls

--- src/app/main_frame_init.cpp
+++ src/app/main_frame_init.cpp
@@ -9,10 +9,9 @@ namespace SecureStore::Application
     MainFrame::MainFrame(const wxString &title, const wxPoint &pos, const wxSize &size)
         : wxFrame((wxFrame *) NULL, -1, title, pos, size)
     {
-        this->btnPasswordEnter = new wxButton(this, BUTTON_PASSWORD_ENTER, _T("Enter"),wxDefaultPosition, wxDefaultSize, 0);
+        this->btnPasswordEnter = new wxButton(this, BUTTON_PASSWORD_ENTER, _T("Enter"), wxPoint(110, 5), wxDefaultSize, 0);
 
-        wxPoint textPosition(100, 0);
-        this->textPasswordCtrl = new wxTextCtrl(this, TEXT_PASSWORD_Entry, _T(""), textPosition, wxDefaultSize, wxTE_PASSWORD, wxDefaultValidator);
+        this->textPasswordCtrl = new wxTextCtrl(this, TEXT_PASSWORD_Entry, _T(""), wxPoint(5, 5), wxSize(100, 20), wxTE_PASSWORD, wxDefaultValidator);
     }
 
     BEGIN_EVENT_TABLE ( MainFrame, wxFrame )
