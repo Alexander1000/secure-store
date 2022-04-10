c028d8b2edbeea930fdbd36661e9c9f6e1b39c9c
Alexander Dankovtsev <a.dankovtsev@mail.ru>
2022-04-10 14:38:24 +0300

    add text box

--- src/app/add_secret/initialize.cpp
+++ src/app/add_secret/initialize.cpp
@@ -12,17 +12,30 @@ namespace SecureStore::Application
         wxPoint wxPosTitleText((this->GetSize().x - this->lbTitleText->GetSize().x) >> 1, 0);
         this->lbTitleText->SetPosition(wxPosTitleText);
 
+        // name
+
         this->lbName = new wxStaticText(this, F_AS_LB_NAME, _T("Name:"));
         this->lbName->SetPosition(wxPoint(5, 20));
+
+        this->txName = new wxTextCtrl(this, F_AS_TX_NAME, wxEmptyString);
+        this->txName->SetPosition(wxPoint(120, 20));
+
+        // user
 
         this->lbUser = new wxStaticText(this, F_AS_LB_USER, _T("User:"));
         this->lbUser->SetPosition(wxPoint(5, 40));
 
+        // password
+
         this->lbPassword = new wxStaticText(this, F_AS_LB_PASSWORD, _T("Password:"));
         this->lbPassword->SetPosition(wxPoint(5, 60));
 
+        // comment
+
         this->lbComment = new wxStaticText(this, F_AS_LB_COMMENT, _T("Comment:"));
         this->lbComment->SetPosition(wxPoint(5, 80));
+
+        // buttons
 
         this->btnSave = new wxButton(this, F_AS_BTN_SAVE, _T("Save"), wxPoint(5, this->GetSize().y - 60));
         this->btnCancel = new wxButton(this, F_AS_BTN_CANCEL, _T("Cancel"), wxPoint(5 + this->btnSave->GetSize().x, this->GetSize().y - 60));
