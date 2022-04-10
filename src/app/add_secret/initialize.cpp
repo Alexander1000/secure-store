e034a23797c9780e9effa258318823705538628b
Alexander Dankovtsev <a.dankovtsev@mail.ru>
2022-04-10 14:47:15 +0300

    set position texts

--- src/app/add_secret/initialize.cpp
+++ src/app/add_secret/initialize.cpp
@@ -9,40 +9,51 @@ namespace SecureStore::Application
     void AddSecretFrame::initialize()
     {
         this->lbTitleText = new wxStaticText(this, F_AS_LB_TITLE_TEXT, _T("Input data for save in db:"));
-        wxPoint wxPosTitleText((this->GetSize().x - this->lbTitleText->GetSize().x) >> 1, 0);
+        wxPoint wxPosTitleText((this->GetSize().x - this->lbTitleText->GetSize().x) >> 1, 5);
         this->lbTitleText->SetPosition(wxPosTitleText);
+        this->lbTitleText->SetFont(wxFont(wxFontInfo().Bold()));
 
         // name
 
+        int yOffset = 30;
+        int yInterval = 25;
+        int nRow = 0;
+
         this->lbName = new wxStaticText(this, F_AS_LB_NAME, _T("Name:"));
-        this->lbName->SetPosition(wxPoint(5, 20));
+        this->lbName->SetPosition(wxPoint(5, yOffset + yInterval * nRow));
 
         this->txName = new wxTextCtrl(this, F_AS_TX_NAME, wxEmptyString);
-        this->txName->SetPosition(wxPoint(120, 20));
+        this->txName->SetPosition(wxPoint(120, yOffset + yInterval * nRow));
 
         // user
 
+        nRow++;
+
         this->lbUser = new wxStaticText(this, F_AS_LB_USER, _T("User:"));
-        this->lbUser->SetPosition(wxPoint(5, 40));
+        this->lbUser->SetPosition(wxPoint(5, yOffset + yInterval * nRow));
 
         this->txUser = new wxTextCtrl(this, F_AS_TX_USER, wxEmptyString);
-        this->txUser->SetPosition(wxPoint(120, 40));
+        this->txUser->SetPosition(wxPoint(120, yOffset + yInterval * nRow));
 
         // password
 
+        nRow++;
+
         this->lbPassword = new wxStaticText(this, F_AS_LB_PASSWORD, _T("Password:"));
-        this->lbPassword->SetPosition(wxPoint(5, 60));
+        this->lbPassword->SetPosition(wxPoint(5, yOffset + yInterval * nRow));
 
         this->txPassword = new wxTextCtrl(this, F_AS_TX_PASSWORD, wxEmptyString);
-        this->txPassword->SetPosition(wxPoint(120, 60));
+        this->txPassword->SetPosition(wxPoint(120, yOffset + yInterval * nRow));
 
         // comment
 
+        nRow++;
+
         this->lbComment = new wxStaticText(this, F_AS_LB_COMMENT, _T("Comment:"));
-        this->lbComment->SetPosition(wxPoint(5, 80));
+        this->lbComment->SetPosition(wxPoint(5, yOffset + yInterval * nRow));
 
         this->txComment = new wxTextCtrl(this, F_AS_TX_COMMENT, wxEmptyString);
-        this->txComment->SetPosition(wxPoint(120, 80));
+        this->txComment->SetPosition(wxPoint(120, yOffset + yInterval * nRow));
 
         // buttons
 
