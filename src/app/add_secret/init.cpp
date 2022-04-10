dade8ae05ffba4d40839f0e105eafb94c5813bf1
Alexander Dankovtsev <a.dankovtsev@mail.ru>
2022-04-10 11:48:05 +0300

    add secret init

---
+++ src/app/add_secret/init.cpp
@@ -0,0 +1,11 @@
+#include <wx/wxprec.h>
+#ifndef WX_PRECOMP
+#	include <wx/wx.h>
+#endif
+#include <secure-store.h>
+#include <memory>
+
+namespace SecureStore::Application
+{
+    // declare form
+}
