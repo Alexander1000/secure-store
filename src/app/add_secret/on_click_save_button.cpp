765444fc44eb2d03e3ec7abfe95d8580ee1e4e1a
Alexander Dankovtsev <a.dankovtsev@mail.ru>
2022-04-11 10:24:47 +0300

    build records

--- src/app/add_secret/on_click_save_button.cpp
+++ src/app/add_secret/on_click_save_button.cpp
@@ -8,6 +8,15 @@ namespace SecureStore::Application
 {
     void AddSecretFrame::OnClickSaveButton(wxCommandEvent &event)
     {
-        // do save
+        SecureStore::Storage::Record* record = nullptr;
+        record = new SecureStore::Storage::Record;
+
+        auto wxName = this->txName->GetValue();
+        record->setName(new std::string(wxName.c_str().AsChar()));
+
+        auto wxUser = this->txUser->GetValue();
+        if (!wxUser.empty()) {
+            record->setUser(new std::string(wxName.c_str().AsChar()));
+        }
     }
 }
