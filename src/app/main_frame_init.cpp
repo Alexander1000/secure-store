802ad41a218d0b8c58142e8e249f627de868ae42
Alexander Dankovtsev <a.dankovtsev@mail.ru>
2022-04-13 09:00:29 +0300

    show ctrls

--- src/app/main_frame_init.cpp
+++ src/app/main_frame_init.cpp
@@ -19,12 +19,28 @@ namespace SecureStore::Application
 
         int yOffset = 0;
 
-        this->lbFileBrowse = new wxStaticText(this, F_AUTH_LB_FILE_BROWSE, _("File:"), wxPoint(xMargin, yMargin));
+        this->lbFileBrowse = new wxStaticText(this, F_AUTH_LB_FILE_BROWSE, _T("File:"), wxPoint(xMargin, yMargin));
         this->txFilePath = new wxTextCtrl(this, F_AUTH_TX_FILE_PATH, wxEmptyString, wxPoint(xMargin + this->lbFileBrowse->GetSize().GetWidth() + xMargin, yMargin));
 
         yOffset = yMargin + 30;
-        this->btnPasswordEnter = new wxButton(this, BUTTON_PASSWORD_ENTER, _T("Enter"), wxPoint(110, yOffset), wxDefaultSize, 0);
-        this->textPasswordCtrl = new wxTextCtrl(this, TEXT_PASSWORD_Entry, _T(""), wxPoint(xMargin, yOffset), wxSize(100, 20), wxTE_PASSWORD, wxDefaultValidator);
+        this->lbPasswordEnter = new wxStaticText(this, F_AUTH_LB_PASSWORD, _T("Password:"), wxPoint(xMargin, yOffset));
+        this->textPasswordCtrl = new wxTextCtrl(
+            this,
+            TEXT_PASSWORD_Entry,
+            _T(""),
+            wxPoint(xMargin + this->lbPasswordEnter->GetSize().GetWidth() + xMargin, yOffset),
+            wxSize(100, 20),
+            wxTE_PASSWORD,
+            wxDefaultValidator
+        );
+        this->btnPasswordEnter = new wxButton(
+            this,
+            BUTTON_PASSWORD_ENTER,
+            _T("Enter"),
+            wxPoint(this->textPasswordCtrl->GetPosition().x + this->textPasswordCtrl->GetSize().GetWidth() + xMargin, yOffset),
+            wxDefaultSize,
+            0
+        );
 
         this->lbPasswordResult = new wxStaticText(this, STATIC_TEXT_PASSWORD_RESULT, wxEmptyString);
         this->lbPasswordResult->Hide();
