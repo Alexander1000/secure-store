4c7438f0e4849beb386a31e0a0a014ae7ea4ebae
Alexander Dankovtsev <a.dankovtsev@mail.ru>
2022-04-20 08:57:33 +0300

    save file

--- src/app/events/on_click_create_file.cpp
+++ src/app/events/on_click_create_file.cpp
@@ -9,6 +9,21 @@ namespace SecureStore::Application
 {
     void MainFrame::OnClickCreateFile(wxCommandEvent &event)
     {
-        // do something
+        auto sOpenDirectory = this->txOpenDirectory->GetValue();
+        auto sFileName = this->txFileName->GetValue();
+        int nPath = sOpenDirectory.size() + sFileName.size() + 2;
+        INIT_CHAR_STRING(sPath, nPath)
+        memcpy(sPath, sOpenDirectory.c_str().AsChar(), sOpenDirectory.size());
+        int offset = sOpenDirectory.size();
+        if (sPath[sOpenDirectory.size() - 1] != '/') {
+            sPath[sOpenDirectory.size()] = '/';
+            offset++;
+        }
+        memcpy(sPath + offset, sFileName.c_str().AsChar(), sFileName.size());
+
+        auto sPassword = this->txPassword->GetValue();
+
+        this->storage = new SecureStore::Storage::DB;
+        this->storage->save(new std::string(sPath), new std::string(sPassword.c_str().AsChar()));
     }
 }
