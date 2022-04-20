3b667c0d3cdbd634e7aa16a0edf87a781a30da13
Alexander Dankovtsev <a.dankovtsev@mail.ru>
2022-04-20 08:22:41 +0300

    select directory

--- src/app/events/on_click_select_directory.cpp
+++ src/app/events/on_click_select_directory.cpp
@@ -9,6 +9,14 @@ namespace SecureStore::Application
 {
     void MainFrame::OnClickSelectDirectory(wxCommandEvent &event)
     {
-        // do something
+        auto browseDirectory = new wxDirDialog;
+        browseDirectory->ShowModal();
+        auto path = browseDirectory->GetPath();
+        if (path.empty()) {
+            this->txOpenDirectory->SetValue(wxEmptyString);
+        } else {
+            this->txOpenDirectory->SetValue(path);
+        }
+        delete browseDirectory;
     }
 }
