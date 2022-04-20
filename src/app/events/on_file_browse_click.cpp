1255f1d28d732dece6126e081598da3b3d883a67
Alexander Dankovtsev <a.dankovtsev@mail.ru>
2022-04-20 10:55:06 +0300

    file filter

--- src/app/events/on_file_browse_click.cpp
+++ src/app/events/on_file_browse_click.cpp
@@ -9,7 +9,7 @@ namespace SecureStore::Application
 {
     void MainFrame::OnFileBrowseClick(wxCommandEvent &event)
     {
-        auto browseFile = new wxFileDialog;
+        auto browseFile = new wxFileDialog(this, _("Open XDB file"), "", "", "XDB files (*.xdb)|*.xdb", wxFD_OPEN | wxFD_FILE_MUST_EXIST);
         browseFile->ShowModal();
         auto sFileName = browseFile->GetFilename();
         this->txFilePath->SetValue(browseFile->GetDirectory() + "/" + sFileName);
