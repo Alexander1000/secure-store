fc1ad65fb9099ac024ec471decf62bb144edb44f
Alexander Dankovtsev <a.dankovtsev@mail.ru>
2022-04-22 16:17:54 +0300

    remove secret from storage

--- src/app/show/events/on_click_remove_secret.cpp
+++ src/app/show/events/on_click_remove_secret.cpp
@@ -8,6 +8,28 @@ namespace SecureStore::Application
 {
     void ShowFrame::OnClickRemoveSecret(wxCommandEvent &event)
     {
-        // do something
+        if (this->selectedSecretIndex < 0
+            || this->storage == nullptr
+            || this->storage->getRecords() == nullptr
+            || this->storage->getRecords()->empty()
+            || this->selectedSecretIndex >= this->storage->getRecords()->size()) {
+            return;
+        }
+
+        int index = 0;
+
+        for (auto & it : *this->storage->getRecords()) {
+            auto record = it->getRecord();
+
+            if (index == this->selectedSecretIndex) {
+                this->storage->getRecords()->remove(it);
+                break;
+            }
+
+            index++;
+        }
+
+        this->storage->save();
+        this->reloadContent();
     }
 }
