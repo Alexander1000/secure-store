a13dcf24cda4574d2e1103cbbb99bf9be4a74120
Alexander Dankovtsev <a.dankovtsev@mail.ru>
2022-04-25 15:14:17 +0300

    make base string

--- src/crypto/prepare_credentials.cpp
+++ src/crypto/prepare_credentials.cpp
@@ -7,6 +7,24 @@ namespace SecureStore::Crypto
 {
     cipher_key* prepare_credentials(const char* user, const char* password, const char* salt)
     {
+        int userLength = strlen(user);
+        if (userLength > 32) {
+            userLength = 32;
+        }
+
+        int passwordLength = strlen(password);
+        if (passwordLength > 32) {
+            passwordLength = 32;
+        }
+
+        int baseStringLength = userLength + 1 + passwordLength + 1 + 32;
+        char* baseString = (char*) malloc(baseStringLength);
+        memset(baseString, 0, baseStringLength);
+
+        memcpy(baseString, user, userLength);
+        memcpy(baseString + userLength, password, passwordLength);
+        memcpy(baseString + userLength + passwordLength, salt, 32);
+
         return nullptr;
     }
 }
