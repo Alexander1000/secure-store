3d4da0f0cd6f41da500376b8b3acfe1c5e7eecd2
Alexander Dankovtsev <a.dankovtsev@mail.ru>
2022-04-25 16:14:42 +0300

    cipher

--- src/crypto/prepare_credentials.cpp
+++ src/crypto/prepare_credentials.cpp
@@ -2,6 +2,7 @@
 
 #include <memory>
 #include <openssl/evp.h>
+#include <io-buffer.h>
 
 namespace SecureStore::Crypto
 {
@@ -27,6 +28,23 @@ namespace SecureStore::Crypto
 
         auto input = new SecureStore::DataPack(baseStringLength, baseString);
         auto hashData = hash_sha3_512(input);
+        IOBuffer::IOMemoryBuffer outBuffer;
+
+        auto cipherType = EVP_bf_cbc();
+        int cipher_block_size = EVP_CIPHER_block_size(cipherType);
+        unsigned char in_buf[BUFSIZE], out_buf[BUFSIZE + cipher_block_size];
+        int num_bytes_read, out_len;
+
+        EVP_CIPHER_CTX *ctx;
+        ctx = EVP_CIPHER_CTX_new();
+        EVP_CipherInit_ex(ctx, EVP_bf_cbc(), NULL, hashData, hashData + 32, 1);
+        EVP_CipherUpdate(ctx, out_buf, &out_len, in_buf, num_bytes_read);
+        outBuffer.write((char*) out_buf, out_len);
+        EVP_CipherFinal_ex(ctx, out_buf, &out_len);
+
+        outBuffer.write((char*) out_buf, out_len);
+
+        EVP_CIPHER_CTX_cleanup(ctx);
 
         return nullptr;
     }
