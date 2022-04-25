c75dbfcabf8c4982982062e76073ccf8915b5f33
Alexander Dankovtsev <a.dankovtsev@mail.ru>
2022-04-25 16:24:36 +0300

    test case

--- tests/src/prepare_credentials/case01.cpp
+++ tests/src/prepare_credentials/case01.cpp
@@ -3,6 +3,7 @@
 #include <cpp-unit-test.h>
 #include <string>
 #include <memory>
+#include <openssl/rand.h>
 
 namespace SecureStoreTest
 {
@@ -11,6 +12,14 @@ namespace SecureStoreTest
         CppUnitTest::TestCase* t = nullptr;
         t = new CppUnitTest::TestCase("testPrepareCredentials_Data_case01");
         t->printTitle();
+
+        const char* user = "alexander";
+        const char* password = "123Qwerty!";
+        char* salt = (char*) malloc(32);
+        RAND_bytes((unsigned char*) salt, 32);
+
+        SecureStore::Crypto::prepare_credentials(user, password, salt);
+
         t->finish();
         return t;
     }
