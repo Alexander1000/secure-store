51464ac6c1663655a27351dd84c20a2dcbd30439
Alexander Dankovtsev <a.dankovtsev@mail.ru>
2022-05-01 17:15:37 +0300

    drop legacy

--- include/secure-store.h
+++ include/secure-store.h
@@ -3,7 +3,7 @@
 
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 0
-#define VERSION_PATCH 1
+#define VERSION_PATCH 2
 
 #include <secure-store/app.h>
 #include <secure-store/app/add_secret.h>
@@ -12,8 +12,6 @@
 #include <secure-store/storage.h>
 #include <secure-store/data-pack.h>
 #include <secure-store/crypto.h>
-#include <secure-store/getpasswd.h>
 #include <secure-store/utils.h>
-#include <secure-store/view.h>
 
 #endif
--- include/secure-store/getpasswd.h
+++
@@ -1,9 +0,0 @@
-#ifndef H_SECURE_STORE_GETPASSWD
-#define H_SECURE_STORE_GETPASSWD
-
-namespace SecureStore
-{
-    ssize_t getpasswd(char **pw, size_t sz, int mask, FILE *fp);
-}
-
-#endif
