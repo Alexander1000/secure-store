0589dead58875d5cc0f1718a3068268aabe97978
Alexander Dankovtsev <a.dankovtsev@mail.ru>
2022-05-15 13:32:41 +0300

    reopen and assert

--- tests/src/save/case03.cpp
+++ tests/src/save/case03.cpp
@@ -22,7 +22,7 @@ namespace SecureStoreTest
         record->setPassword(&recPassword);
         std::string comment = "It is example record in secure store - for test case testSaveDb_AppendData_case03";
         record->setComment(&comment);
-        record->setCreateTime(9734827842);
+        record->setCreateTime(973482784);
 
         db->addRecord(record);
         db->save();
@@ -32,5 +32,25 @@ namespace SecureStoreTest
 
         int result = db->open(fileName.c_str(), password.c_str());
         CppUnitTest::assertEquals(t, 0, result);
+
+        auto recs = db->getRecords();
+        CppUnitTest::assertNotNull(t, recs);
+        CppUnitTest::assertEquals(t, 1, recs->size());
+
+        auto itRecs = recs->begin();
+
+        auto dbRecord = *itRecs;
+        CppUnitTest::assertEquals(t, 0, dbRecord->getStatus());
+
+        record = dbRecord->getRecord();
+        CppUnitTest::assertNotNull(t, record);
+
+        CppUnitTest::assertEquals(t, 1, record->getId());
+        CppUnitTest::assertEquals(t, "testSaveDb_AppendData_case03_record1", record->getName());
+        CppUnitTest::assertEquals(t, "tutu", record->getUser());
+        CppUnitTest::assertEquals(t, "flkjliw85", record->getPassword());
+        CppUnitTest::assertEquals(t, "It is example record in secure store - for test case testSaveDb_AppendData_case03", record->getComment());
+        CppUnitTest::assertNull(t, record->getKeywords());
+        CppUnitTest::assertEquals(t, 973482784, record->getCreateTime());
     END_TEST_CASE()
 }
