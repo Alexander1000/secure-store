e5e067cce707f1e06c5a7025ef1fd357387a2296
Alexander Dankovtsev <a.dankovtsev@mail.ru>
2022-05-15 13:22:27 +0300

    update test case

--- tests/src/save/case03.cpp
+++ tests/src/save/case03.cpp
@@ -11,5 +11,21 @@ namespace SecureStoreTest
 
         SecureStore::Storage::DB db;
         db.save(fileName.c_str(), password.c_str());
+
+        auto record = new SecureStore::Storage::Record;
+        record->setId(1);
+        std::string name = "testSaveDb_AppendData_case03_record1";
+        record->setName(&name);
+        std::string user = "tutu";
+        record->setUser(&user);
+        std::string recPassword = "flkjliw85";
+        record->setPassword(&recPassword);
+        std::string comment = "It is example record in secure store - for test case testSaveDb_AppendData_case03";
+        record->setComment(&comment);
+        record->setCreateTime(9734827842);
+
+        db.addRecord(record);
+        db.save();
+
     END_TEST_CASE()
 }
