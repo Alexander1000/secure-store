1d2e6a7a2c6c66b9833a2546672a05e47fd5987b
Alexander Dankovtsev <a.dankovtsev@mail.ru>
2022-05-15 14:16:42 +0300

    fix segfault

--- src/app/switch_to_show_frame.cpp
+++ src/app/switch_to_show_frame.cpp
@@ -9,13 +9,13 @@ namespace SecureStore::Application
 {
     void MainFrame::switchToShowFrame()
     {
-        this->getApp()->getMainFrame()->Close();
         auto showFrame = (ShowFrame*) this->getApp()->getShowFrame();
         showFrame->storage = this->storage;
         INIT_CHAR_STRING(sTitle, 15 + strlen(this->_fileName)); // "Secure store: " // 14
         sprintf(sTitle, "Secure store: %s", this->_fileName);
         showFrame->SetTitle(_(sTitle));
         showFrame->renderStorageRecords();
+        this->getApp()->getMainFrame()->Close();
         showFrame->Show(true);
     }
 }
