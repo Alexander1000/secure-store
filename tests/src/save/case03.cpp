79cc4e69a2e4ae2bafc9a96320a559f9b947f8d9
Alexander Dankovtsev <a.dankovtsev@mail.ru>
2022-05-15 13:19:22 +0300

    save empty file

--- tests/src/save/case03.cpp
+++ tests/src/save/case03.cpp
@@ -6,5 +6,10 @@
 namespace SecureStoreTest
 {
     BEGIN_TEST_CASE(testSaveDb_AppendData_case03, "save db - append data in storage")
+        std::string fileName = "testSaveDb_AppendData_case03.xdb";
+        std::string password = "bigOlolo5224^%@";
+
+        SecureStore::Storage::DB db;
+        db.save(fileName.c_str(), password.c_str());
     END_TEST_CASE()
 }
