75d5caa3a30aa9d652477c6fbc62edb9d92d7d1b
Alexander Dankovtsev <a.dankovtsev@mail.ru>
2022-05-15 13:40:39 +0300

    finalize test case

--- tests/src/save/case03.cpp
+++ tests/src/save/case03.cpp
@@ -75,6 +75,48 @@ namespace SecureStoreTest
         db->addRecord(record);
         db->save();
 
+        delete db;
+        db = new SecureStore::Storage::DB;
 
+        // STEP: open early saved file and assert result
+
+        result = db->open(fileName.c_str(), password.c_str());
+        CppUnitTest::assertEquals(t, 0, result);
+
+        recs = db->getRecords();
+        CppUnitTest::assertNotNull(t, recs);
+        CppUnitTest::assertEquals(t, 2, recs->size());
+
+        itRecs = recs->begin();
+
+        dbRecord = *itRecs;
+        CppUnitTest::assertEquals(t, 0, dbRecord->getStatus());
+
+        record = dbRecord->getRecord();
+        CppUnitTest::assertNotNull(t, record);
+
+        CppUnitTest::assertEquals(t, 1, record->getId());
+        CppUnitTest::assertEquals(t, "testSaveDb_AppendData_case03_record1", record->getName());
+        CppUnitTest::assertEquals(t, "tutu", record->getUser());
+        CppUnitTest::assertEquals(t, "flkjliw85", record->getPassword());
+        CppUnitTest::assertEquals(t, "It is example record in secure store - for test case testSaveDb_AppendData_case03", record->getComment());
+        CppUnitTest::assertNull(t, record->getKeywords());
+        CppUnitTest::assertEquals(t, 973482784, record->getCreateTime());
+
+        // second record
+        itRecs++;
+        dbRecord = *itRecs;
+        CppUnitTest::assertEquals(t, 0, dbRecord->getStatus());
+        record = dbRecord->getRecord();
+        CppUnitTest::assertNotNull(t, record);
+        CppUnitTest::assertEquals(t, 2, record->getId());
+        CppUnitTest::assertEquals(t, "testSaveDb_AppendData_case03_record2", record->getName());
+        CppUnitTest::assertEquals(t, "super_user_double_dragon", record->getUser());
+        CppUnitTest::assertEquals(t, "nbduierw", record->getPassword());
+        CppUnitTest::assertEquals(t, "This is secret for me (testSaveDb_AppendData_case03)", record->getComment());
+        CppUnitTest::assertNull(t, record->getKeywords());
+        CppUnitTest::assertEquals(t, 978472, record->getCreateTime());
+
+        delete db;
     END_TEST_CASE()
 }
